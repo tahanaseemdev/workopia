f90b114ba5bc24f465cb40b4c99d777070829897
Taha <taha.naseem589@gmail.com>
2025-07-31 12:18:33 +0500

    Implemented a mobile hamburger menu, Created a bottom banner component, Developed a hero section component, Added a top banner component.

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,16 +1,8 @@
 @props(['url'=> '/','active'=>false,'icon'=>null,'mobile'=>null])
-@if($mobile)
-    <a href="{{$url}}" class="block px-4 py-2 hover:bg-blue-700 {{$active ? 'text-yellow-500 font-bold' :''}}">
-    @if($icon)
-        <i class="fa fa-{{$icon}} mr-1"></i>
-    @endif
-        {{$slot}}
-    </a>
-@else
-<a href="{{$url}}" class="text-white hover:underline py-2 {{$active ? 'text-yellow-500 font-bold' :''}}">
+
+<a href="{{$url}}" class="{{$mobile ? 'block px-4 py-2 hover:bg-blue-700' : 'text-white hover:underline py-2'}} {{$active ? 'text-yellow-500 font-bold' :''}}">
     @if($icon)
         <i class="fa fa-{{$icon}} mr-1"></i>
     @endif
     {{$slot}}
 </a>
-@endif
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,12 +6,18 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	@vite('resources/css/app.css')
+	<link rel="stylesheet" href="{{asset('css/style.css')}}">
 	<title>{{$title ?? 'Workopia | Find and list jobs'}}</title>
 </head>
-<body class="bg=gray-100">
+<body class="bg-gray-100">
 	<x-header />
+	@if(request()->is('/'))
+	<x-hero />
+	<x-top-banner />
+	@endif
 	<main class="container mx-auto p-4 mt-4">
 		{{$slot}}
 	</main>
+	<script src="{{asset('js/script.js')}}"></script>
 </body>
 </html>
