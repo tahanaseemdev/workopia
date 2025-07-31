3c2b7b9a28ee915aeb21f2c2437c5f2e8b01e635
Taha <taha.naseem589@gmail.com>
2025-07-31 11:36:24 +0500

    Add Tailwind CSS Created a button-link Blade component. Created a nav-link Blade component.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,8 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	@vite('resources/css/app.css')
 	<title>{{$title ?? 'Workopia | Find and list jobs'}}</title>
 </head>
 <body class="bg=gray-100">
